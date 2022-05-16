fa9e34ccf9bf8f1de9752dcaddb9c80506cd9d59
NULL-header <poaoq.poaoq@gmail.com>
2022-05-16 17:36:00 +0900

    update parse base for error handling

--- injectable_macro/src/parse/error.rs
+++ injectable_macro/src/parse/error.rs
@@ -1,18 +1,26 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum ParseError {
+pub enum ParseErrorKind {
     #[error("The inject macro can not use with an empty trait.")]
     Empty,
     #[error("The code is not a trait, or it is wrong with syntax.")]
     NotTrait,
 }
 
-impl ParseError {
-    pub fn make_syn_error<T>(self, token: Option<T>) -> syn::Error
-    where
-        T: quote::ToTokens,
-    {
-        syn::Error::new_spanned(token, self.to_string())
+pub struct ParseError<'a> {
+    kind: ParseErrorKind,
+    token: Option<Box<dyn quote::ToTokens + 'a>>,
+}
+
+impl<'a> std::convert::Into<syn::Error> for ParseError<'a> {
+    fn into(self) -> syn::Error {
+        syn::Error::new_spanned(self.token, self.kind.to_string())
+    }
+}
+
+impl<'a> ParseError<'a> {
+    pub fn new(kind: ParseErrorKind, token: Option<Box<dyn quote::ToTokens + 'a>>) -> Self {
+        ParseError { kind, token }
     }
 }
---
+++ injectable_macro/src/parse/input.rs
@@ -0,0 +1,71 @@
+use super::error::{ParseError, ParseErrorKind};
+
+#[derive(Debug)]
+pub struct Input {
+    body: syn::ItemTrait,
+}
+
+fn parse_input(input: syn::parse::ParseStream) -> Result<Input, ParseError> {
+    let body: syn::ItemTrait = match input.parse() {
+        Ok(b) => b,
+        _ => {
+            let kind = ParseErrorKind::NotTrait;
+            let error = ParseError::new(kind, None);
+            return Err(error);
+        }
+    };
+    if body.attrs.len() == 0 {
+        let kind = ParseErrorKind::Empty;
+        let token = Box::new(body);
+        let error = ParseError::new(kind, Some(token));
+        return Err(error);
+    }
+    Ok(Input { body })
+}
+
+impl syn::parse::Parse for Input {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        match parse_input(input) {
+            Err(e) => Err(e.into()),
+            Ok(i) => Ok(i),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use quote::quote;
+
+    #[test]
+    fn not_trait() {
+        let input = quote! {
+            1+1
+        };
+        let result: syn::Result<Input> = syn::parse2(input);
+        match result {
+            Ok(_) => {
+                panic!("unknown error");
+            }
+            Err(e) => {
+                assert_eq!(e.to_string(), ParseErrorKind::NotTrait.to_string());
+            }
+        }
+    }
+
+    #[test]
+    fn empty() {
+        let input = quote! {
+          trait Mock {}
+        };
+        let result: syn::Result<Input> = syn::parse2(input);
+        match result {
+            Ok(_) => {
+                panic!("unknown error");
+            }
+            Err(e) => {
+                assert_eq!(e.to_string(), ParseErrorKind::Empty.to_string());
+            }
+        }
+    }
+}
