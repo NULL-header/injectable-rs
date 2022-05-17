9d25f3938f8b50e972d1a58f93cd413247f6d4b1
NULL-header <poaoq.poaoq@gmail.com>
2022-05-18 08:18:40 +0900

    feat fields of input

--- injectable_macro/src/parse/input.rs
+++ injectable_macro/src/parse/input.rs
@@ -3,7 +3,19 @@ use super::error::{ParseError, ParseErrorKind};
 #[derive(Debug)]
 pub struct Input {
   body: syn::ItemTrait,
-  tmp: Vec<syn::TraitItem>,
+}
+
+impl Input {
+  pub fn make_fields(&self) -> Vec<&syn::Ident> {
+    let mut fields: Vec<&syn::Ident> = Vec::new();
+    self.body.items.iter().for_each(|e| match e {
+      syn::TraitItem::Method(e) => {
+        fields.push(&e.sig.ident);
+      }
+      _ => {}
+    });
+    fields
+  }
 }
 
 fn parse_input(input: syn::parse::ParseStream) -> Result<Input, ParseError> {
@@ -21,10 +33,7 @@ fn parse_input(input: syn::parse::ParseStream) -> Result<Input, ParseError> {
     let error = ParseError::new(kind, Some(token));
     return Err(error);
   }
-  Ok(Input {
-    body: body.clone(),
-    tmp: body.items,
-  })
+  Ok(Input { body })
 }
 
 impl syn::parse::Parse for Input {
@@ -74,7 +83,22 @@ mod test {
   }
 
   #[test]
-  fn tmp() {
-    panic!("holder");
+  fn fields() {
+    let input = quote! {
+      trait Mock {
+        fn some();
+      }
+    };
+    let result: syn::Result<Input> = syn::parse2(input);
+    match result {
+      Ok(i) => {
+        let fields = i.make_fields();
+        let some = fields[0].to_string();
+        assert_eq!(some, "some");
+      }
+      Err(_) => {
+        panic!("unknown error");
+      }
+    };
   }
 }
