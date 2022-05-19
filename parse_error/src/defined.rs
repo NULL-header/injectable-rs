d6528b6f73784645b897f8d817aa812c6b99994e
NULL-header <poaoq.poaoq@gmail.com>
2022-05-20 02:29:33 +0900

    feat trait for derive macro

--- parse_error/src/defined.rs
+++ parse_error/src/defined.rs
@@ -1,3 +1,7 @@
-pub trait ToSynError {
-  fn to_syn_error() {}
+use proc_macro2::Span;
+
+pub trait ToSynError: std::fmt::Display {
+  fn to_syn_error(&self, span: Span) -> syn::Error {
+    syn::Error::new(span, self.to_string())
+  }
 }
